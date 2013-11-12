a7548a40df4d5be76a2ccdb19fd4de8894898e82
gil.maricruz@gmail.com <gil.maricruz@gmail.com@b104a250-5576-1979-6155-f292f37a00f3>
2013-11-12 17:04:13 +0000

    se crea clase Disco

---
+++ Disco.py
@@ -0,0 +1,16 @@
+'''
+Created on 24/10/2013
+
+@author: Griselda, Maricruz
+'''
+
+class Disco:
+    def __init__(self):
+        self.programas = []
+
+    def addProgram(self,p):
+        self.programa.append(p)
+
+    def getPrograma(self,nomProg):
+    #busco el programa en la lista
+        return self.programa(nomProg)   
