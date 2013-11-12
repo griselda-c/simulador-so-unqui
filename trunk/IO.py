eb1c9ed59328acbbe378b17d1ae9b0f199e500c0
gil.maricruz@gmail.com <gil.maricruz@gmail.com@b104a250-5576-1979-6155-f292f37a00f3>
2013-11-12 17:05:50 +0000

    se crea clase IO

---
+++ trunk/IO.py
@@ -0,0 +1,36 @@
+'''
+Created on 24/10/2013
+
+@author: Griselda, Maricruz
+'''
+
+import threading
+from threading import Semaphore
+import time
+from miFifo import *
+from IRQExitIO import *
+
+
+class IO(threading.Thread):
+    def __init__(self,irqManager):
+        threading.Thread.__init__(self)
+        self.cola = miFifo()
+        self.irqManager = irqManager
+
+    def addInstruccion(self,tupla):
+        print("an instruction is added to the queue of isIO\n")
+        self.cola.addElement(tupla)
+        
+    def existInstruction(self):
+        return self.cola.size() > 0
+
+
+    def run(self):
+        while True:
+            if self.existInstruction():
+                tupla = self.cola.getElement()
+                instruction = tupla[0]
+                print(instruction.message)
+                #lanza un alerta irqExistIO para avisar al kernel que el pcb ya salio de IO
+                irqExistIo = IRQExitIO()
+                self.irqManager.handle(irqExistIo, tupla[1])
