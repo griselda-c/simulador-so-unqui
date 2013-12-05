1d4918aacd7d0b2076f231b534b5a1a3ff2b4f35
gil.maricruz@gmail.com <gil.maricruz@gmail.com@b104a250-5576-1979-6155-f292f37a00f3>
2013-12-05 05:04:47 +0000

    Marco

---
+++ branches/Marco.py
@@ -0,0 +1,8 @@
+
+
+class Marco:
+    def __init__(self,direInicial, tamanio):
+        self.direInicial = direInicial
+        for index in range(0,tamanio):
+			self.celdas[index] = None
+        
