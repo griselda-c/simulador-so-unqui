9a48b516849f4e5399f0d99548ebd0b12a280fb1
gil.maricruz@gmail.com <gil.maricruz@gmail.com@b104a250-5576-1979-6155-f292f37a00f3>
2013-12-06 03:34:04 +0000

    Memoria

--- branches/Memory.py
+++ branches/Memory.py
@@ -1,47 +1,44 @@
 
 from Block import *
+from Marco import *
 
 class Memory:
     def __init__(self, mode, limit):
-    #a validar
-        self.celdas = {} # las celdas es ahora un diccionario
-        # modo: tipos de asignacion (continua o paginacion)
-        self.mode = mode
-        self.mode.blockFree.append(Block(0,limit - 1)) #bloque inicial, la memoria entera
-        #limit es la capacidad total de la memoria
+        self.celdas = {}
+        self.mode = mode #PAGINACION
         self.limit = limit
-        # Necesito crear la memoria fisica
+        self.marcosLibres = []
+        self.marcosOcupados = []
+
+		# Se crean las celdas de memoria vacia
         for index in range(0,self.limit):
 			self.celdas[index] = None
-			
-            
+
+		# Se crean los marcos de celdas
+        while index < self.limit:
+			marco = Marco(index,self.mode.tamanioMarco)
+			index = index + self.mode.tamanioMarco
+			self.marcosLibres.append(marco)
+
     def addInstruction(self,index,instruction):
         print("en el indice " +str(index) + " se guardo instruccion " +str(instruction)+"\n")
         self.celdas[index] = instruction
-        
-    def getCantCeldasLibres(self):
-        return self.limit - len(self.celdas)
-        
+
     def hayLugar(self,tamanio):   # LO LLAMA EL PLP
-		return self.mode.hayLugar(tamanio,self.limit,self.celdas)
+		return len(self.marcosLibres) > 0
   
     def load(self,programa,pcb):    # LO LLAMA EL PLP
-        block = self.mode.findBlockEmpty(programa.getCantInst(),self)#agrego aca la memoria
-        pcb.baseDirection = block.first # se le asigna la direccionBase
-        index = block.first
-        for instruccion in programa.instrucciones:
-            instruccion.setPcb(pcb)
-            self.addInstruction(index, instruccion)
-            index = index + 1       
-        print("se cargo el programa en memoria\n")
-            
+        marco = self.mode.findBlockEmpty(programa,pcb,self)#agrego aca la memoria
+        self.mode.cargarPagina(pcb,marco)
+
     def delete(self,pcb): # LO LLAMA EL KERNEL
         for direction in range(pcb.baseDirection,pcb.baseDirection+pcb.cantInst):
             del self.celdas[direction]
             print("Se libero la celda----> "+str(direction)+" del pcb ---->" +str(pcb.pid)+"\n")
         print("Borre de memoriaaaaaaaaaaaaaaaaaaa\n")
         self.mode.liberarBloque(pcb.baseDirection)#buscar el bloque
-    
+
+'''
     # Utilizado para la paginacion
     def existeMarcoLibre(self,tamanioMarco):
         contCeldas = 0
@@ -58,3 +55,13 @@ class Memory:
 				resultado = True
         print("existe MARCO LIBRE: "+str(resultado))
         return resultado
+'''
+'''		
+        pcb.baseDirection = block.first # se le asigna la direccionBase
+        index = block.first
+        for instruccion in programa.instrucciones:
+            instruccion.setPcb(pcb)
+            self.addInstruction(index, instruccion)
+            index = index + 1       
+        print("se cargo el programa en memoria\n")
+'''
