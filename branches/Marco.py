d2cdfeef2a2942d9d9992c470cbd551a837cc491
gil.maricruz@gmail.com <gil.maricruz@gmail.com@b104a250-5576-1979-6155-f292f37a00f3>
2013-12-06 03:33:32 +0000

    Marco

--- branches/Marco.py
+++ branches/Marco.py
@@ -2,6 +2,7 @@
 
 class Marco:
     def __init__(self,direInicial, tamanio):
+        self.celdas = {}
         self.direInicial = direInicial
         for index in range(0,tamanio):
 			self.celdas[index] = None
